b639a147e9dc0904727872685e6569219ad3914e
informsmijith <smijith@hummingsoft.com.my>
2015-11-25 13:03:01 +0530

    Update LoginAction.java

--- SMA_Extended/src/com/sma_x/master/LoginAction.java
+++ SMA_Extended/src/com/sma_x/master/LoginAction.java
@@ -21,7 +21,7 @@ package com.sma_x.master;
 * Copyright @ 2015-2016 
 * All Rights Reserved.
 *  --------------------------------------------------------
-* |-----------SummerSoft Private Limited ------------------| 
+* |-----------SummerSoft Private Ltd ------------------| 
 *  --------------------------------------------------------
 *   B9,9th Floor,Heavenly Plaza,Vazhakala,Kakanad-682021
 ************************************************************
